1086281e8755637d873dd7f94c1763f7f5ec1bec
24juice22 <mike.james.nunn@gmail.com>
2022-09-19 15:28:09 -0400

    Altered Visual of Code
    Altered how code in "box" component looks

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,14 +1,19 @@
 import React from "react"
 
 export default function Box({boxes, columnIndex, rowIndex, position, error}) {
-    const letter = boxes[rowIndex][columnIndex]
+    const letter = boxes[rowIndex][columnIndex];
     
     return (
         <div 
             className={error && rowIndex === position.rowIndex ? 
-                        "box error" : position.rowIndex > rowIndex ? 
-                        "box boxFlip" : "box"}
-            id={letter.color}>{letter.value}
+                        "box error" : 
+                        position.rowIndex > rowIndex ? 
+                        "box boxFlip" : 
+                        "box"
+            }
+            id={letter.color}
+        >
+            {letter.value}
         </div>
     )
 }
