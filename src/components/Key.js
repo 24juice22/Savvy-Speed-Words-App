d5b1101bc38dcbe143315990e48d463e55fb57cb
24juice22 <mike.james.nunn@gmail.com>
2022-09-23 11:44:03 -0400

    Key and Keyboard Component More DRY
    Moved insertLetter function from "key" to "keyboard" component and just used a callback. As a result many props being passed to "key" were removed. Added semicolons.

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,24 +1,10 @@
 import React from "react";
 
-export default function Key({boxes, setBoxes, keyValue, position, setPosition, hasStarted, keyColor}) {
-    let keyIndex = keyColor.map(item => item.value).indexOf(keyValue);
+export default function Key({letter, keyColor, insertLetter}) {
+    let keyIndex = keyColor.map(item => item.value).indexOf(letter);
     let insertColor = keyColor[keyIndex].color;
-    
-    function insertLetter() {
-        if (hasStarted !== true) return;
-        if (position.columnIndex > 4) return;
-        const board = [...boxes];
-        board[position.rowIndex][position.columnIndex].value = keyValue;
-        setBoxes(board);
-        setPosition(prevPosition => {
-            return {
-                ...prevPosition, 
-                columnIndex: prevPosition.columnIndex + 1
-            }
-        });
-    }
-        
+     
     return (
-        <div className={`key key-letter ${insertColor}`} onClick={insertLetter}>{keyValue}</div>
+        <div className={`key key-letter ${insertColor}`} onClick={() => insertLetter(letter)}>{letter}</div>
     )
 }
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,104 +1,131 @@
-import React from "react"
-import Key from "./Key"
+import React from "react";
+import Key from "./Key";
 
-export default function Keyboard({boxes, setBoxes, position, setPosition, words, word, toggleErrorShake, colors, hasStarted, gameOver, setMessage, keyColor, timeIntervalID, setGameOver, delayedGameOver}) {  
-    const newBoard = [...boxes]
-    let wordEntered = (newBoard[position.rowIndex].map(object => object.value).join(""))
-
-    const keysTop = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]
-    const keysMiddle = ["A", "S", "D", "F", "G", "H", "J", "K", "L"]
-    const keysBottom = ["Z", "X", "C", "V", "B", "N", "M"]
+export default function Keyboard({wordEntered, boxes, setBoxes, position, setPosition, words, word, toggleErrorShake, colors, hasStarted, gameOver, setMessage, keyColor, timeIntervalID, delayedGameOver}) {  
   
+    const keysTop = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+    const keysMiddle = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+    const keysBottom = ["Z", "X", "C", "V", "B", "N", "M"];
+  
+    ////// Insert Chosen Letter into Boxes /////////////
+    function insertLetter(letter) {
+        if (hasStarted !== true) return;
+        if (position.columnIndex > 4) return;
+        const board = [...boxes];
+        board[position.rowIndex][position.columnIndex].value = letter;
+        setBoxes(board);
+        setPosition(prevPosition => {
+            return {
+                ...prevPosition, 
+                columnIndex: prevPosition.columnIndex + 1
+            }
+        });
+    }
+    
+    ///// Make Keyboard Functional /////////////
     const handleKeyboard = React.useCallback((event) => {
-        function insert(key) {
-            let newKey = key.toLowerCase()
-            if (event.key === newKey) {
-                if (hasStarted !== true) return
-                if (position.columnIndex > 4) return
-                const board = [...boxes]
-                board[position.rowIndex][position.columnIndex].value = key
-                setBoxes(board)
-                setPosition(prevPosition => {
-                    return {
-                        ...prevPosition, 
-                        columnIndex: prevPosition.columnIndex + 1
-                    }
-                })
-            } 
+        function insert(letter) {
+            let newLetter = letter.toLowerCase();
+            if (event.key === newLetter) 
+                insertLetter(letter)
         }
 
         if (event.key === "Enter" && !gameOver) {
-            enterWord()
+            enterWord();
         } 
         else if (event.key === "Backspace" || event.key === "Delete") {
-            deleteLetter()
+            deleteLetter();
         }
         else {
             keysTop.forEach((key) => {
-                insert(key)
+                insert(key);
             })
             keysMiddle.forEach((key) => {
-                insert(key)
-             })
+                insert(key);
+            })
              keysBottom.forEach((key) => {
-                insert(key)
-             })
+                insert(key);
+            })
         }
     })
 
+    //////// Listen for Keydown and Clean Up ////////
     React.useEffect(() => {
-        document.addEventListener("keydown", handleKeyboard)
+        document.addEventListener("keydown", handleKeyboard);
         return () => {
-            document.removeEventListener("keydown", handleKeyboard)
+            document.removeEventListener("keydown", handleKeyboard);
         }
     }, [handleKeyboard])
-
+    //////////////////////////////////////
+    
     function deleteLetter() {
-        if (position.columnIndex === 0) return 
-        newBoard[position.rowIndex][position.columnIndex - 1].value = ""
-        setBoxes(newBoard)
+        let newBoard = [...boxes];
+        if (position.columnIndex === 0) return; 
+        newBoard[position.rowIndex][position.columnIndex - 1].value = "";
+        setBoxes(newBoard);
         setPosition(prevPosition => {
             return { 
                 ...prevPosition,
                 columnIndex: prevPosition.columnIndex - 1,
             }
-        })
+        });
     }
 
     function enterWord() {
-      if (position.columnIndex !== 5) return
-      checkIfValidWord()  
+      if (position.columnIndex !== 5) return;
+      checkIfValidWord();  
     }
 
     function checkIfValidWord() {
         if (words.includes(wordEntered)) {
             if (word !== wordEntered && position.rowIndex !== 5)
-                setPosition({rowIndex: position.rowIndex + 1, columnIndex: 0})
-            colors()
+                setPosition({rowIndex: position.rowIndex + 1, columnIndex: 0});
+            colors();
             if (word !== wordEntered && position.rowIndex === 5) {
-                setMessage(`You lose! The word was "${word}"`)
-                clearInterval(timeIntervalID)
-                setTimeout(delayedGameOver, 1200)
+                setMessage(`You lose! The word was "${word}"`);
+                clearInterval(timeIntervalID);
+                setTimeout(delayedGameOver, 1200);
             }
         }
         else {
-            toggleErrorShake()
-            setTimeout(toggleErrorShake, 610)
+            toggleErrorShake();
+            setTimeout(toggleErrorShake, 610);
         }
     }
 
     return (
         <div className="keyboard" onKeyDown={handleKeyboard}>
-            <div className="flex keyboard-top">
-                {keysTop.map(letter => <Key hasStarted={hasStarted} position={position} setPosition={setPosition} setBoxes={setBoxes} boxes={boxes} keyValue={letter} key={letter} keyColor={keyColor} />)}
+            <div className="keyboard__row">
+                {keysTop.map(letter => {
+                    return <Key 
+                                letter={letter} 
+                                key={letter} 
+                                keyColor={keyColor} 
+                                insertLetter={insertLetter}
+                            />
+                })}
             </div>
-            <div className="flex keyboard-middle">
-                {keysMiddle.map(letter => <Key hasStarted={hasStarted} position={position} setPosition={setPosition} setBoxes={setBoxes} boxes={boxes} keyValue={letter} key={letter} keyColor={keyColor}/>)}
+            <div className="keyboard__row">
+                {keysMiddle.map(letter => {
+                    return <Key 
+                                letter={letter} 
+                                key={letter} 
+                                keyColor={keyColor}
+                                insertLetter={insertLetter}
+                            />
+                })}
             </div>
-            <div className="flex keyboard-bottom">
-                <div className="key key-special key-enter" onClick={enterWord}>ENT</div>
-                {keysBottom.map(letter => <Key hasStarted={hasStarted} position={position} setPosition={setPosition} setBoxes={setBoxes} boxes={boxes} keyValue={letter} key={letter} keyColor={keyColor}/>)}
-                <div className="key key-special key-delete" onClick={deleteLetter} >DEL</div>
+            <div className="keyboard__row">
+                <div className="key key--special key--enter" onClick={enterWord}>ENT</div>
+                {keysBottom.map(letter => {
+                    return <Key 
+                                letter={letter} 
+                                key={letter} 
+                                keyColor={keyColor}
+                                insertLetter={insertLetter}
+                            />
+                })}
+                <div className="key key--special key--delete" onClick={deleteLetter} >DEL</div>
             </div>
         </div> 
     )
