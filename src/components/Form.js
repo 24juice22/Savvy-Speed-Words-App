95e3f71b489464cec85fd7504d952eb1ab7e0755
24juice22 <mike.james.nunn@gmail.com>
2022-09-23 11:48:46 -0400

    Form Tag Only Surrounds Inputs Now
    The form tag does not envelope the entire component now.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,19 @@
-import React from "react"
+import React from "react";
 
 export default function Form({setName}) {
     function handleSubmit(event) {
-        event.preventDefault()
-        setName(event.target[0].value)
+        event.preventDefault();
+        setName(event.target[0].value);
     }
 
     return (
-        <form className="form" onSubmit={handleSubmit}>
+        <div className="form">
             <h2 className="form__title">PLEASE ENTER YOUR NAME</h2>
             <p>(Maximum of 14 Characters)</p>
-            <input className="form__input" type="text" maxLength="14"/>
-            <input className="btn form__btn" type="submit" value="SUBMIT" />
-        </form>
+            <form onSubmit={handleSubmit}>
+                <input className="form__input" type="text" maxLength="14" required/>
+                <input className="btn form__btn" type="submit" value="SUBMIT" />
+            </form>
+        </div>
     )
 }
