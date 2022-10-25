82a76bd4eb3a36dcc889868566dd2bd3a02951a5
24juice22 <mike.james.nunn@gmail.com>
2022-10-25 14:20:54 -0400

    Wrap ChooseGame in Container
    Wrap chooseGame elements in a container.

--- src/components/Choosegame.js
+++ src/components/Choosegame.js
@@ -5,19 +5,21 @@ export default function Choosegame() {
 
     return (
         <div className="chooseGame">
-            <h1 className="chooseGame__main-title">SA<span>VV</span>Y SPEED WORDS</h1>
-            <h2 className="chooseGame__title">CHOOSE THE NUMBER OF WORDS</h2>
-            <h3 className="chooseGame__directions-title">
-                You will be timed on how long it takes you to correctly guess the number 
-                of words of your choosing.
-            </h3> 
-            <p className="chooseGame__directions-text">
-                EXAMPLE: If you choose "3", once you correctly guess the first word, 
-                the board will reset and you will have a new word to guess. The timer 
-                will continue until you have correctly guessed all 3 words 
-                (or until you fail a word, then you lose!)
-            </p>
-            <Buttons />
+            <div className="container">
+                <h1 className="chooseGame__main-title">SA<span>VV</span>Y SPEED WORDS</h1>
+                <h2 className="chooseGame__title">CHOOSE THE NUMBER OF WORDS</h2>
+                <h3 className="chooseGame__directions-title">
+                    You will be timed on how long it takes you to correctly guess the number 
+                    of words of your choosing.
+                </h3> 
+                <p className="chooseGame__directions-text">
+                    EXAMPLE: If you choose "3", once you correctly guess the first word, 
+                    the board will reset and you will have a new word to guess. The timer 
+                    will continue until you have correctly guessed all 3 words 
+                    (or until you fail a word, then you lose!)
+                </p>
+                <Buttons />
+            </div>
         </div>
     )
 }
